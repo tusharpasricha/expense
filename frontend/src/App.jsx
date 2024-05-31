fa20ccb4c3ebb5fd7e3071c426914de7cf5cf8cb
Tushar Pasricha <tusharpasricha21@gmail.com>
2024-05-31 14:10:45 +0530

    head

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,21 +4,22 @@ import Accounts from './mycomp/accounts'
 import Expense from './mycomp/expense'
 import Income from './mycomp/income'
 import Categories from './mycomp/category'
-
+import Head from './mycomp/head'
 function App() {
 
   return (
     <div className='mineouter'>
-    <div className='flex flex-row  justify-around mineinner' >
-      <Expense/>
-      <Income/>
-      <div className='mineinner2'> 
-      <Accounts/>
-      <Categories/>
+      <Head/>
+      <div className='flex flex-row  justify-around mineinner' >
+        <Expense />
+        <Income />
+        <div className='mineinner2'>
+          <Accounts />
+          <Categories />
+        </div>
+
       </div>
-      
-    </div>
-    <List/>
+      <List />
     </div>
   )
 }
---
+++ frontend/src/mycomp/head.jsx
@@ -0,0 +1,12 @@
+
+
+function Head() {
+    return (
+        <div className="flex text-7xl justify-center">
+            Expanse Tracker
+        </div>
+
+    )
+}
+
+export default Head;
